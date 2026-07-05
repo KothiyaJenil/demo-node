5db1e180138165fe62e9611a8a5ee362d9a28dc4
KothiyaJenil <jenilkothiya.dev@gmail.com>
2026-07-05 15:35:20 +0530

    update code

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 import express from "express";
-import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DBinstance } from "./config/db";
 
-dotenv.config({ quiet: true });
 
-const app = express();
+export const app = express();
 
 
 app.use(express.json()); // parse and work with json data
@@ -18,10 +16,4 @@ app.get("/", async (req, res) => {
   const user = db.collection("admins");
   const data = await user.find().toArray();
   res.status(201).json({ data })
-});
-
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`server start
-http://localhost:${process.env.PORT || 3000}`);
 });
